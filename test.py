16e2c7b601990fab02514b3cde1c289143f840d9
Christopher Kyle Horton <christhehorton@gmail.com>
2015-10-06 17:10:16 -0400

    Add initial code files.

---
+++ test.py
@@ -0,0 +1,109 @@
+from stanford_corenlp_pywrapper import CoreNLP
+
+print("************************************************** Loading CoreNLP **************************************************")
+proc = CoreNLP("parse")
+print("************************************************** CoreNLP loaded! **************************************************")
+
+
+
+def ParseText(text):
+	return proc.parse_doc(text)
+
+
+def GetVerb(parsed):
+	for word in range(len(parsed["tokens"])):
+		# found a verb
+		if parsed["pos"][word].startswith("V"):
+			# found an adverb / determiner
+			if word > 0 and parsed["pos"][word-1].startswith("W"):
+				return (word-1, word+1) #"%s %s" % (parsed["tokens"][word-1], parsed["tokens"][word])
+			else:
+				return (word, word+1) #parsed["tokens"][word]
+			#(parsed["lemmas"][word], parsed["tokens"][word], parsed["pos"][word])
+
+def FindDependency(parsed, verbPos, depType):
+	for dep in parsed["deps_basic"]:
+		if dep[0] == depType and dep[1] in range(verbPos[0], verbPos[1]):
+			return (dep[2], dep[2]+1)
+
+def GetSubject(parsed, verbPos):
+	# Try and do some actual sentence parsing, looking for the subject
+	ret = FindDependency(parsed, verbPos, "nsubj")
+	if ret:
+		return ret
+	ret = FindDependency(parsed, verbPos, "dobj")
+	if ret:
+		return ret
+
+	# That failed, just return the first noun it finds
+	nounStart = None
+	for word in range(len(parsed["tokens"][verbPos[1]:])):
+		if nounStart:
+			if not parsed["pos"][word].startswith("N"):
+				return (nounStart, word)
+		elif parsed["pos"][word].startswith("N"):
+			nounStart = word
+	if nounStart:
+		return (nounStart, len(parsed["tokens"])-1)
+
+# Get Extra data, checking for prepositions that modify things
+def GetExtra(parsed, verbPos):
+	newVerb = newSubject = None
+	prep = FindDependency(parsed, verbPos, "prep")
+	if not prep:
+		print("Searching for xcomp")
+		newVerb = FindDependency(parsed, verbPos, "xcomp")
+		prep = FindDependency(parsed, newVerb, "prep")
+	if prep:
+		newSubject = FindDependency(parsed, prep, "pobj")
+		if not newSubject:
+			# not sure if this is possible
+			print("Error parsing preposition")
+			return
+		time = FindDependency(parsed, newSubject, "num")
+		if time and time[0] < newSubject[1]:
+			newSubject = (time[0], newSubject[1])
+
+		#newVerb = FindDependency(parsed, prep, "dobj")
+	return newSubject, newVerb, prep
+		
+	
+
+def GetWords(parsed, wordPositions):
+	if not wordPositions:
+		return ""
+	return " ".join(parsed["tokens"][wordPositions[0]:wordPositions[1]])
+
+def Parse(text):
+	parsed = ParseText(text)["sentences"]
+	verb = noun = newNoun = newVerb = prep = None
+	for sentence in parsed:
+		print(sentence)
+		verb = GetVerb(sentence)
+		if verb:
+			print(GetWords(sentence, verb))
+			noun = GetSubject(sentence, verb)
+			if noun:
+				print(GetWords(sentence, noun))
+			else:
+				print("Could not find the subject")
+
+			newNoun, newVerb, prep = GetExtra(sentence, verb)
+			if newNoun:
+				print("New Noun: " + GetWords(sentence, newNoun))
+			if newVerb:
+				print("New Verb: " + GetWords(sentence, newVerb))
+			if prep:
+				print("Prep: " + GetWords(sentence, prep))
+		else:
+			print("Could not find any verbs")
+		# just return the first sentence only for now
+		print("\n")
+		return GetWords(sentence, verb), GetWords(sentence, noun), GetWords(sentence, newNoun), GetWords(sentence, newVerb), GetWords(sentence, prep)
+	
+
+
+
+# http://www.comp.leeds.ac.uk/amalgam/tagsets/upenn.html  <- parts of speech reference
+# http://nlp.stanford.edu/software/dependencies_manual.pdf <- dependencies reference
+# https://github.com/brendano/stanford_corenlp_pywrapper
