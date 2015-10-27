af3846308b1671479fe4f6f78238c3b273f48abf
Khalil <kkhalaf@ltu.edu>
2015-10-27 16:00:53 -0400

    Added the function that retrieves the weather information + called it from assistantdb weahther case

--- web.py
+++ web.py
@@ -2,6 +2,7 @@
 import urllib, urllib2
 import re
 
+# this is the function that will retrieve the source code of an url
 def GetPage(url, cookies = None, headers = None, removeTags = False, getredirect=False):
     try:
         if cookies:
@@ -16,3 +17,11 @@ def GetPage(url, cookies = None, headers = None, removeTags = False, getredirect
         return url if getredirect else page
     except IOError:
         return None
+
+# this is the function that retrieves the weath information
+def GetWeatherInfo():
+    page = GetPage("http://www.wunderground.com/q/zmw:48033.1.99999?MR=1")
+    degreestart = page.find("Southfield, MI | ")
+    degrees = page[degreestart+17:degreestart+21]
+    status = page[degreestart+29:page.find("\"", degreestart+29)]
+    return degrees, status
