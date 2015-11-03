91bef38864cc9e5b0b807b5ee7fa2ae0b52672f3
Christopher Kyle Horton <christhehorton@gmail.com>
2015-11-03 14:39:33 -0500

    Add basic write function to calendar CSV file.

--- calendardb.py
+++ calendardb.py
@@ -8,6 +8,12 @@ def read_events():
     with open('calendar.csv', 'rb') as calendar_csv:
         calreader = csv.reader(calendar_csv, delimiter=',', quotechar='"')
         for row in calreader:
-            print ', '.join(row)
+            print(', '.join(row))
             event_list.append(row)
     return event_list
+
+def add_event(event_item_list):
+    '''Adds a new event in list form to the calender DB CSV file.'''
+    with open('calendar.csv', 'wb') as calendar_csv:
+        calwriter = csv.writer(calendar_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        calwriter.writerow(event_item_list)
