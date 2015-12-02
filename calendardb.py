562b58a10364c86e3496a8ca6f807a02d76e0a3c
Christopher Kyle Horton <christhehorton@gmail.com>
2015-12-02 14:51:46 -0500

    Store CalendarEvent.date as an actual datetime object and add function to get only today's events.

--- calendardb.py
+++ calendardb.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-import csv
+import csv, datetime
 
 class CalendarEvent():
     '''Class for storing calendar event information.'''
     def __init__(self, event_str='', date_str='', start_time_str='', end_time_str=''):
         '''Initialize this CalendarEvent instance.'''
         self.event_str = event_str
-        self.date_str = date_str
+        self.date = datetime.strptime(date_str, '%B %d %Y')
         self.start_time_str = start_time_str
         self.end_time_str = end_time_str
 
@@ -21,10 +21,19 @@ def read_events():
             event_list.append(CalendarEvent(row[0], row[1], row[2], row[3]))
     return event_list
 
+def get_todays_events():
+    '''Returns a list of CalendarEvents scheduled for today.'''
+    event_list = read_events()
+    todays_date = datetime.today.date()
+    return [event for event in event_list if event.date == todays_date]
+
 def add_event(ce):
     '''Adds a new event in list form to the calendar DB CSV file.
     Takes a CalendarEvent object.'''
     with open('calendar.csv', 'wb') as calendar_csv:
         calwriter = csv.writer(calendar_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        cal_row = [ce.event_str, ce.date_str, ce.start_time_str, ce.end_time_str]
+        cal_row = [ce.event_str,
+                   ce.date.strftime('%B %d %Y'),
+                   ce.start_time_str,
+                   ce.end_time_str]
         calwriter.writerow(cal_row)
